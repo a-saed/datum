2a811f089a42e706b4ef01e7840cfc928e798729
Abdulrhman Elsaed <abdoelsaed114@gmail.com>
2026-05-27 20:00:45 +0300

    chore: revert demo deployment, host demo locally only for now

--- demo/vite.config.ts
+++ demo/vite.config.ts
@@ -3,8 +3,7 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: process.env.VITE_BASE_URL ?? '/',
-  optimizeDeps: {
+optimizeDeps: {
     exclude: ['@electric-sql/pglite', '@electric-sql/pglite-postgis'],
   },
 })
